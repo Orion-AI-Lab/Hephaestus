be857a0f57daa9fc388876e0ca5e2b4a85575b1a
ngbountos <mpountos@outlook.com.gr>
2023-02-16 12:03:25 +0200

    Add functionality for loading distributed data parallel checkpoint for finetuning

--- utilities/utils.py
+++ utilities/utils.py
@@ -1,14 +1,28 @@
 import json
 from pathlib import Path
 
+import self_supervised.mocov2.builder as builder
+import timm
+import torch
+import torch.distributed as dist
+import torch.nn as nn
 
-def create_checkpoint_directory(args):
-    checkpoint_path = (
-        Path("checkpoints")
-        / args["method"].lower()
-        / args["architecture"].lower()
-        / f'{args["architecture"].lower()}_{str(args["resolution"])}'
-    )
+
+def create_checkpoint_directory(args,wandb_run=None):
+    if wandb_run is None:
+        checkpoint_path = (
+            Path("checkpoints")
+            / args["method"].lower()
+            / args["architecture"].lower()
+            / f'{args["architecture"].lower()}_{str(args["resolution"])}'
+        )
+    else:
+        checkpoint_path = (
+            Path("checkpoints") / wandb_run
+            / args["method"].lower()
+            / args["architecture"].lower()
+            / f'{args["architecture"].lower()}_{str(args["resolution"])}'
+        )
     checkpoint_path.mkdir(parents=True, exist_ok=True)
     return checkpoint_path
 
@@ -36,3 +50,26 @@ def prepare_configuration(path):
     base_cfg["checkpoint_path"] = checkpoint_path.as_posix()
 
     return base_cfg
+
+def load_encoder(checkpoint_path,config=None):
+    if config is None:
+        config_path = "configs/configs.json"
+        config = prepare_configuration(config_path)
+        config['pretrained']=False
+    checkpoint = torch.load(checkpoint_path,map_location='cpu')
+    if config["method"] == "mocov2":
+        model = builder.MoCo(
+            config,
+            config["moco_dim"],
+            config["moco_k"],
+            config["moco_m"],
+            config["moco_t"],
+            config["mlp"])
+        print(model)
+        dist.init_process_group(backend="nccl")
+
+        model = nn.parallel.DistributedDataParallel(model.cuda())
+        model.load_state_dict(checkpoint['state_dict'])
+        model.module.encoder_q.fc = nn.Identity()
+        torch.save(model.module.encoder_q,'pretrained_encoder_'+config['architecture']+'.pt')
+        return model.module.encoder_q
