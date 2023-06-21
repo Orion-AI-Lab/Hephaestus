c7a54823dd6d671eb6fec3aaf18d7488d4b87d8a
ngbountos <mpountos@outlook.com.gr>
2023-06-21 21:12:43 +0300

    add command line arguments that can overwrite supervised configs

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-import json
+import pyjson5 as json
 from self_supervised.mocov2 import builder
 from utilities.utils import prepare_configuration
 from training import train_ssl, train_supervised
 import argparse
+import pprint
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--supervised', action='store', type=bool, default=True)
+    parser.add_argument('--ssl_encoder_path', action='store', type=str, default=None)
+    parser.add_argument('--ssl_config_path', action='store', type=str, default=None)
+
     args = parser.parse_args()
     if args.supervised:
         config_path = "configs/supervised_configs.json"
         configs = json.load(open(config_path,'r'))
+        if args.ssl_encoder_path is not None and args.ssl_config_path is not None:
+            configs['ssl_encder'] = args.ssl_encoder_path
+            configs['ssl_config_path'] = args.ssl_config_path
+        print('Initializing supervised training with configs: ')
+        pprint.pprint(configs)
         train_supervised.train(configs)
     else:
         # Parse configurations
